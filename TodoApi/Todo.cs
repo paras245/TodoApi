c2306c03cba0bf1bb5d8b689c642ea276e0c7880
Paras Panchal <paraspanchal5555@gmail.com>
2023-11-28 22:03:06 +0530

    Add project files.

---
+++ TodoApi/Todo.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApi
+{
+
+    //Access Modifier Class ClassName
+    public class Todo
+    {
+        //Access Modifier Type Entity  
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
